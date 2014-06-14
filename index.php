7178023071d95d9006908d17213e957f79930597
mytory <mytory@gmail.com>
2014-06-14 20:49:25 +0900

    계정 목록 나오게 함.

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $client = new Google_Client();
 $client->setApplicationName('newspaper ga report');
 $client->setClientId('921572832525-3qbloc0n6pmil1iporo2deu78f5jbups.apps.googleusercontent.com');
 $client->setClientSecret('Ao421TNJ0Q5fvD2HlxSD1US-');
-$client->setRedirectUri('http://localhost/_MASTER/ga.php');
+$client->setRedirectUri('http://localhost/_MASTER/index.php');
 $client->setDeveloperKey('AIzaSyCmHnBdJZluJyKN-I2BpVxQ94TWXoMYpaA');
 $client->setScopes(array('https://www.googleapis.com/auth/analytics.readonly'));
 
@@ -29,7 +29,8 @@ if (!$client->getAccessToken()) {
     exit;
 }
 $service = new Google_Service_Analytics($client);
-$accounts = $service->management_accounts->listManagementAccounts();
+$management_accounts = $service->management_accounts->listManagementAccounts();
+$accounts = $management_accounts->getItems();
 ?><!doctype html>
 <html lang="ko">
 <head>
@@ -38,10 +39,14 @@ $accounts = $service->management_accounts->listManagementAccounts();
 </head>
 <body>
     <h1>구글 아날리틱스 신문 통계</h1>
-    <pre>
+    <ul>
     <?php
-    print_r($accounts);
-    $service->management_profiles->get()
-    ?>
+    foreach ($accounts as $account) { ?>
+        <li>
+            <a href="profiles.php?account_id=<?php echo $account->id ?>"><?php echo $account->name?></a>
+            (<a href="<?php echo $account->selfLink ?>">GA에서 보기</a>)
+        </li>
+    <?php } ?>
+    </ul>
 </body>
 </html>
