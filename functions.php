f923375af6bf7e306fb1c2790692aa6623670141
mytory <mytory@gmail.com>
2014-06-15 19:06:22 +0900

    소스,매체별 방문.

--- functions.php
+++ functions.php
@@ -2,27 +2,24 @@
 /**
  * 변수의 구성요소를 리턴받는다.
  */
-function getPrintr($var, $title = null)
-{
+function getPrintr($var, $title = null) {
     $dump = '';
-    $dump .=  '<div align="left">';
-    $dump .=  '<pre style="background-color:#000; color:#00ff00; padding:5px; font-size:14px;">';
-    if( $title )
-    {
-        $dump .=  "<strong style='color:#fff'>{$title} :</strong> \n";
+    $dump .= '<div align="left">';
+    $dump .= '<pre style="background-color:#000; color:#00ff00; padding:5px; font-size:14px;">';
+    if ($title) {
+        $dump .= "<strong style='color:#fff'>{$title} :</strong> \n";
     }
     $dump .= print_r($var, TRUE);
-    $dump .=  '</pre>';
-    $dump .=  '</div>';
-    $dump .=  '<br />';
+    $dump .= '</pre>';
+    $dump .= '</div>';
+    $dump .= '<br />';
     return $dump;
 }
 
 /**
  * 변수의 구성요소를 출력한다.
  */
-function printr($var, $title = null)
-{
+function printr($var, $title = null) {
     $dump = getPrintr($var, $title);
     echo $dump;
 }
@@ -30,8 +27,98 @@ function printr($var, $title = null)
 /**
  * 변수의 구성요소를 출력하고 멈춘다.
  */
-function printr2($var, $title = null)
-{
+function printr2($var, $title = null) {
     printr($var, $title);
     exit;
+}
+
+function ga_get_source_medium($start_date, $end_date){
+    global $analytics;
+    $results = $analytics->data_ga->get(
+        'ga:' . $_GET['profile_id'],
+        $start_date,
+        $end_date,
+        'ga:sessions,ga:pageviews',
+        array(
+            'dimensions' => 'ga:source,ga:medium',
+            'sort' => '-ga:pageviews'
+        )
+    );
+    return $results;
+}
+
+/**
+ * 특정 기간 총 방문수와 페이지뷰
+ * @param $start_date
+ * @param $end_date
+ * @return mixed
+ */
+function ga_get_total($start_date, $end_date){
+    global $analytics;
+    $results = $analytics->data_ga->get(
+        'ga:' . $_GET['profile_id'],
+        $start_date,
+        $end_date,
+        'ga:sessions,ga:pageviews'
+    );
+    return $results;
+}
+
+function ga_print_column($column){
+    if(is_numeric($column)){
+        return number_format($column);
+    }else if(strstr($column, '.')){
+        return "<a target='_blank' href='http://{$column}'>{$column}</a>";
+    }else{
+        return $column;
+    }
+}
+
+function ga_print_table($results, $title = ''){
+    $headers = $results->getColumnHeaders();
+    $rows = $results->getRows();
+    $sessions = $results->getTotalsForAllResults()['ga:sessions'];
+    if(isset($results->getTotalsForAllResults()['ga:pageviews'])){
+        $pageviews = $results->getTotalsForAllResults()['ga:pageviews'];
+    }
+    ?>
+    <div class="row clearfix">
+        <?php if($title) { ?>
+            <h2><?php echo $title ?></h2>
+        <?php } ?>
+        <table class="table">
+            <thead>
+            <tr>
+                <?php foreach ($headers as $header) { ?>
+                    <th><?php echo $header['name'] ?></th>
+                <?php } ?>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            $index = 0;
+            foreach ($rows as $row) {
+                $index++;
+                ?>
+                <tr style="<?php echo $index > 10 ? 'display: none' : '' ?>">
+                    <?php foreach ($row as $key => $column) { ?>
+                        <td class="<?php echo is_numeric($column) ? "text-right" : "" ?>">
+                            <?php echo ga_print_column($column) ?>
+                            <?php if($headers[$key]['name'] == 'ga:sessions'){ ?>
+                                <small>(<?php echo round($column/$sessions*100.0, 2) ?>%)</small>
+                            <?php } ?>
+                            <?php if($headers[$key]['name'] == 'ga:pageviews'){ ?>
+                                <small>(<?php echo round($column/$pageviews*100.0, 2) ?>%)</small>
+                            <?php } ?>
+                        </td>
+                    <?php } ?>
+                </tr>
+            <?php } ?>
+            </tbody>
+        </table>
+        <?php if(count($rows) > 10){ ?>
+            <button class="pull-right btn btn-default" onclick="$(this).prev().find('tr:hidden').show(); $(this).remove()">+ 다 보기</button>
+        <?php } ?>
+    </div>
+    <?php
 }
--- report.php
+++ report.php
@@ -1,20 +1,16 @@
 <?php include "default.php" ?>
 <?php include "header.php" ?>
 <?php
+// 날짜, 기간
 $start_date = ( isset($_GET['start_date']) ? $_GET['start_date'] : date('Y-m-d', time() - (60*60*24*7)) );
 $end_date = ( isset($_GET['end_date']) ? $_GET['end_date'] : date('Y-m-d', time() - (60*60*24*1)) );
-$results = $analytics->data_ga->get(
-    'ga:' . $_GET['profile_id'],
-    $start_date,
-    $end_date,
-    'ga:sessions'
-);
+$date_range = ( ( strtotime($end_date) - strtotime($start_date) ) / ( 60*60*24 ) ) + 1;
 
-if(count($results->getRows()) > 0){
-    $profile_name = $results->getProfileInfo()->getProfileName();
-    $rows = $results->getRows();
-    $sessions = $rows[0][0];
-}
+// 매체별 방문수, 페이지뷰
+$source_medium_results = ga_get_source_medium($start_date, $end_date);
+$profile_name = $source_medium_results->getProfileInfo()->profileName;
+$sessions = $source_medium_results->getTotalsForAllResults()['ga:sessions'];
+$pageviews = $source_medium_results->getTotalsForAllResults()['ga:pageviews'];
 ?>
 <h1><?php echo $profile_name ?> 통계</h1>
 <p>
@@ -23,6 +19,32 @@ if(count($results->getRows()) > 0){
     ~
     <?php echo date('Y-m-d(l)', strtotime($end_date)) ?>
 </p>
-<p>총 방문수 <?php echo number_format($sessions) ?>회</p>
+<ul>
+    <li>총 방문수 : <?php echo number_format($sessions) ?>회 (하루 평균 <?php echo number_format(round($sessions / $date_range)) ?>회)</li>
+    <li>총 조회수 : <?php echo number_format($pageviews) ?>회 (하루 평균 <?php echo number_format(round($pageviews / $date_range)) ?>회)</li>
+    <li>방문당 조회수 : <?php echo round($pageviews/$sessions, 2) ?>페이지</li>
+</ul>
 
+<?php ga_print_table($results = $analytics->data_ga->get(
+    'ga:' . $_GET['profile_id'],
+    $start_date,
+    $end_date,
+    'ga:sessions',
+    array(
+        'dimensions' => 'ga:medium',
+        'sort' => '-ga:sessions'
+    )
+), '매체별 방문'); ?>
+<?php ga_print_table($results = $analytics->data_ga->get(
+    'ga:' . $_GET['profile_id'],
+    $start_date,
+    $end_date,
+    'ga:sessions',
+    array(
+        'dimensions' => 'ga:source',
+        'sort' => '-ga:sessions'
+    )
+), '소스별 방문'); ?>
+
+<?php ga_print_table($source_medium_results, '소스와 매체별 방문') ?>
 <?php include "footer.php" ?>
