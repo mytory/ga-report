91b5fb8d3c6ec0e5b97d4b1e3491da750620a3ca
mytory <mytory@gmail.com>
2014-11-23 14:15:43 +0900

    .

--- header.php
+++ header.php
@@ -18,6 +18,8 @@
         margin-bottom: 1em;
     }
     </style>
+    <script src="bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 </head>
 <body>
 <header class="navbar navbar-static-top">
--- report.php
+++ report.php
@@ -31,6 +31,21 @@ $pageviews = $source_medium_results->getTotalsForAllResults()['ga:pageviews'];
     </div>
 </div>
 
+<script src="bower_components/select2/select2.min.js"></script>
+<link rel="stylesheet" href="bower_components/select2/select2.css"/>
+<link rel="stylesheet" href="bower_components/select2/select2-bootstrap.css"/>
+<script>
+    $('#date-range').select2({
+        width: 200
+    }).on('change', function(e){
+        var date_range = e.val.split('~');
+        $('[name="start_date"]').val(date_range[0]);
+        $('[name="end_date"]').val(date_range[1]);
+        $('[name="custom_range_title"]').val(e.added.text);
+    });
+</script>
+
+
 <div class="row">
     <div class="col-md-12">
         <ul>
